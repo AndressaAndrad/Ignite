080e801f8e9591c2412131f0e5da64e3cdd3cefe
Andressa Andrade <andressaandrade.ads@gmail.com>
2022-11-09 12:07:45 -0400

    post CPF

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -1,33 +1,34 @@
-const express = require('express')
+const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/courses", (req, res) => {
-    const query = req.query;
-    console.log(query)
-    return res.json("Curso 1, Curso 2, Curso 3")
-})
+const custumers = [];
 
-app.post("/courses", (req, res) => {
-    const body = req.body;
-    console.log(body)
-    return res.json("Curso 1, Curso 2, Curso 3, Curso 4")
-})
 
-app.put("/courses/:id", (req, res) => {
-    const params = req.params;
-    console.log(params)
-    return res.json("Curso 6, Curso 2, Curso 3, Curso 4")
-})
+app.post("/account", (req, res) => {
+    const { cpf, name } = req.body;
 
-app.patch("/courses/:id", (req, res) => {
-    return res.json("Curso 6, Curso 7, Curso 3, Curso 4")
-})
+    const costumerAlreadyExists = custumers.some(
+        (custumers) => custumers.cpf === cpf
+    );
+        if (costumerAlreadyExists) {
+            return res.status(400).json( 
+                {error: "Customer already exists!"});
+        }
 
-app.delete("/courses/:id", (req, res) => {
-    return res.json("Curso 6, Curso 2, Curso 4")
-})
 
-app.listen(3333)       
+    custumers.push({
+        cpf,
+        name,
+        id: uuidv4(), 
+        statement: []
+    });
+
+    return res.status(201).send();
+});
+
+
+app.listen(3333)
