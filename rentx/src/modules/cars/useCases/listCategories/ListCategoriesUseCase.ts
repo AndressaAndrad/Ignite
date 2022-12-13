32deeff4460f3b44e24411edafcb6654d14fd785
Andressa Andrade <aandrade@psgtecnologia.com.br>
2022-12-13 13:13:57 -0400

    trocando nome rentx

---
+++ rentx/src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,14 @@
+import { Category } from '../../entities/Category';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+class ListCategoriesUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  execute(): Promise<Category[]> {
+    const categories = this.categoriesRepository.null;
+
+    return categories;
+  }
+}
+
+export { ListCategoriesUseCase };
