96fc67f7d8c7aff0ff3770d02183cd4523bae15f
Andressa Andrade <aandrade@psgtecnologia.com.br>
2022-12-30 08:51:00 -0400

    Criando a tabela user

---
+++ rentalx/src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,25 @@
+import { Repository, getRepository } from 'typeorm';
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { User } from '../../entities/Users';
+import { IUsersRepository } from '../IUsersRepository';
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+  async create({ name, username, email, driver_licensess, password }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      username,
+      email,
+      driver_licensess,
+      password,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UsersRepository };
