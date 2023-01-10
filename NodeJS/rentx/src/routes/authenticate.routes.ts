95a9802c4564433aaa9da010f8b2790b545a201c
Andressa Andrade <aandrade@psgtecnologia.com.br>
2023-01-10 08:10:30 -0400

    Organizando

---
+++ NodeJS/rentx/src/routes/authenticate.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { AuthenticateUserController } from '../modules/accounts/UseCases/authenticateUser/AuthenticateUserController';
+
+const authenticateRoutes = Router();
+
+const createAuthenticateController = new AuthenticateUserController();
+authenticateRoutes.post('/sessions', createAuthenticateController.handle);
+
+export { authenticateRoutes };
